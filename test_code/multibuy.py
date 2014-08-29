e6d0abf50adbd2d7bec118c0620059c7dc825210
robert <rwb0000@gmail.com>
2014-08-29 10:10:49 +0100

    order placing munge in a separate diectory; called munge

--- test_code/multibuy.py
+++ test_code/multibuy.py
@@ -21,11 +21,11 @@ from time import strptime
 import datetime
 from datetime import date
 
-test = pd.read_csv('/home/mint/workinprogress/Global_Code/test_code/tesco_test.csv')
+test = pd.read_csv('/home/robert/project_2/supermarket_scraper/output/tesco/tesco_test.csv')
 testDF = DataFrame(test)
 counter = len(testDF)
 print('input data count: ',counter)
 
 testDF['multibuy_ANY'] = testDF['offer'].str.contains("Any")
 testDF['multibuy_BUY'] = testDF['offer'].str.contains("Buy")
-testDF.to_csv('/home/mint/workinprogress/Global_Code/test_code/tesco_multibuy.csv', index = False)
+testDF.to_csv('/home/robert/project_2/supermarket_scraper/output/tesco/tesco_multibuy.csv', index = False)
