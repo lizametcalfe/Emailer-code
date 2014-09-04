ca33f1e3eb9661a916c03870f29cc08bbcae3b4d
dark <you@example.com>
2014-09-04 11:00:15 +0100

    adding on NAS code to the finder

--- Munge/call.py
+++ Munge/call.py
@@ -12,15 +12,14 @@ import datetime
 from datetime import date
 from time import strptime
 
-
-def caller(supermarket,dates):
+def caller(supermarket,dates,long_term_store,scraper_output,dictionary):
 	# 1. This imports the scraper data 
-	raw_data_object = finder.Raw_finder(supermarket,dates)
+	raw_data_object = finder.Raw_finder(supermarket,dates,scraper_output,dictionary)
 	print(raw_data_object)
 	# this is the supermarket data 
 	supermarketDF = raw_data_object.supermarket_finder()
 	counter =  len(supermarketDF)
-	supermarketDF.to_csv('/home/mint/longditudal/output/'+supermarket+'/test_read_1.csv', index = False)
+	supermarketDF.to_csv(long_term_store+supermarket+'/test_read_1.csv', index = False)
 	print("raw count: ", counter)
 
 	# 2. Manipulation of the Raw_data; initialise the object
@@ -30,17 +29,16 @@ def caller(supermarket,dates):
 	supermarketDF = manipulation_object.munge_1()
 	counter =  len(supermarketDF)
 	print("count after manipulation:" ,counter)
-	supermarketDF.to_csv('/home/mint/longditudal/output/'+supermarket+'/test_read_2.csv', index = False)
+	supermarketDF.to_csv(long_term_store+supermarket+'/test_read_2.csv', index = False)
 
 	# 3. Merge with the dictionary; initialise the object
-	merged_object = finder.Merger(supermarket,supermarketDF,dates)
+	merged_object = finder.Merger(supermarket,supermarketDF,dates,dictionary)
 	print(merged_object)
 	# Dictionary and supermarket data merged 
 	supermarket2 = merged_object.combine_data()
 	counter = len(supermarket2)
 	print ('Processed: final count after merging: ', counter) 
-	supermarket2.to_csv('/home/mint/longditudal/output/'+supermarket+'/test_read_3.csv', index = False)
-
+	supermarket2.to_csv(long_term_store+supermarket+'/test_read_3.csv', index = False)
 
 	# 4. Apply the functions; to create new variables 
 	New_variables_object = transformation_functional.string_feeder(supermarket,supermarket2)
@@ -49,11 +47,24 @@ def caller(supermarket,dates):
 	final = New_variables_object.feeder()
 	counter = len(final)
 	print ('Processed: final count after creating new variables: ', counter) 
-	final.to_csv('/home/mint/longditudal/output/'+supermarket+'/test_read_4.csv', index = False)
+	final.to_csv(long_term_store+supermarket+'/test_read_4.csv', index = False)
+
+	# 5. longterm storage 
+	supermarket2 = final 
+	longterm_storage_object = finder.Longterm(supermarket,supermarket2,long_term_store)
+	print(longterm_storage_object)
+	backup = longterm_storage_object.long_series()
+
 
 #"dates" User to change dates otherwise todaty's date will be used
 dates = datetime.date.strftime(date.today(), '%Y%m%d')
+#long term storage, such as the NAS
+long_term_store = '/home/dark/work/longterm/'
+#direct output from the scrapers 
+scraper_output = '/home/dark/work/project_3/supermarket_scraper/output/'
+#dictionary 
+dictionary = '/home/dark/work/project_1/global_code/dictionary'
 
-caller('waitrose', dates)
-caller('tesco', dates)
-caller('sainsbury', dates)
+caller('waitrose', dates,long_term_store,scraper_output,dictionary)
+#caller('tesco', dates,long_term_store,scraper_output,dictionary)
+#caller('sainsbury', dates,long_term_store,scraper_output,dictionary)
--- Munge/finder.py
+++ Munge/finder.py
@@ -24,9 +24,11 @@ from datetime import date
 class Raw_finder(object):
 	#after self are the attributes 
 
-	def __init__(self,supermarket,date):
+	def __init__(self,supermarket,date,scraper_output,dictionary):
 		self.supermarket = supermarket
-		self.date = date 
+		self.date = date
+		self.scraper_output = scraper_output
+		self.dictionary = dictionary 
 
 	def __str__(self):
 		printer = "supermarket object\n"
@@ -38,10 +40,11 @@ class Raw_finder(object):
 	    # attributes 
 	    supermarket = self.supermarket
 	    date = self.date 
+	    scraper_output = self.scraper_output
 	    filename = str(supermarket)+str('_products_')+str(date)+str('*')
 	    print filename
 	    counter = 0
-	    for file in os.listdir('/home/mint/workinprogress/supermarket_scraper/output/'+supermarket+'/'):
+	    for file in os.listdir(scraper_output + supermarket+'/'):
 	    
 	        if fnmatch.fnmatch(file, filename):
 	              
@@ -50,12 +53,12 @@ class Raw_finder(object):
 	            time = file[atpos+1:stopos]
 	        
 	            if counter == 0:
-	                file2 = '/home/mint/workinprogress/supermarket_scraper/output/'+ supermarket + '/'+ file
+	                file2 = scraper_output + supermarket+ '/'+ file
 	                time_before = file[atpos+1:stopos]
 	                print "file equals ", file2
 	                
 	            elif int(time) > int(time_before):
-	                file2 = '/home/mint/workinprogress/supermarket_scraper/output/'+ supermarket + '/'+ file
+	                file2 = scraper_output + supermarket + '/'+ file
 	                time_before = file[atpos+1:stopos]
 	                print "file equals ", file2
 	            
@@ -73,10 +76,11 @@ class Raw_finder(object):
 
 class Merger(object):
 
-	def __init__(self, supermarket, supermarketDF,date):
+	def __init__(self, supermarket, supermarketDF,date,dictionary):
 		self.supermarket = supermarket
 		self.supermarketDF = supermarketDF 
 		self.date = date 
+		self.dictionary = dictionary 
 
 	def __str__(self):
 		counter =  len(self.supermarketDF)
@@ -86,11 +90,12 @@ class Merger(object):
 		return printer 
 
 	def combine_data(self):
-		match_dictionary = pd.read_csv('/home/mint/workinprogress/Global_Code/dictionary/dictionary_1.csv')
+		dictionary = self.dictionary
+		match_dictionary = pd.read_csv(dictionary + '/dictionary.csv')
 		match_dictionary = DataFrame(match_dictionary)
 		match_dictionary['ons_item_name'] = match_dictionary['ons_item_name'].map(lambda x: x.strip())
-		counter = len(match_dictionary)
-		print('dictionary size: ',counter)
+		#counter = len(match_dictionary)
+		#print('dictionary size: ',counter)
 		supermarket = self.supermarket
 		supermarketDF = self.supermarketDF
 		supermarketDF['ons_item_name'] = supermarketDF['ons_item_name'].map(lambda x: x.strip())
@@ -105,4 +110,63 @@ class Merger(object):
 		return supermarket2
 		
 			
-			
+class Longterm(object):
+	
+	def __init__(self,supermarket,supermarket2,long_term_store):	
+		self.supermarket = supermarket
+		self.supermarket2 = supermarket2
+		self.fil = long_term_store
+
+	def __str__(self):
+		#counter =  len(self.supermarketDF)
+		printer = "supermarket long term series object\n"
+		printer += "supermarket:\n " + self.supermarket + "\n" + "backup to longterm storage:\n " 
+		return printer
+
+	def long_series(self):
+		supermarket = self.supermarket
+		fil = self.fil
+		supermarket2 = self.supermarket2
+		counter = len(supermarket2)
+		print('read in file for long term storage:',  counter)
+	'''
+        try:
+            oldfile = pd.read_csv(fil+ supermarket + '/_longitudinal.csv')
+            
+            L = len(oldfile)
+            
+            if L > 0: 
+            	print "L equals ", L
+                
+                #Make a back up of today's file to NAS before appending new data
+                oldfile.to_csv(fil + supermarket + '/_longitudinal_backup.csv', index = False)
+                
+                print 'oldfile = ', len(oldfile)
+               
+                #Check to make sure that the new data is not already in the file, for e.g. if rerunning code
+                if  not (len(oldfile[oldfile['date'] == date[:4]+'_'+date[4:6]+'_'+date[6:]]) == 0 ):
+                    print 'duplicate date'
+                    # deletes current version of date from the file
+                    oldfile = oldfile[oldfile['date'] != date[:4]+'_'+date[4:6]+'_'+date[6:]]
+                    
+                newfile = oldfile.append(supermarket2, ignore_index = True)
+                    
+                newfile.sort(columns=('ons_item_name','product_name', 'date') ,axis = 0, ascending = True, inplace = True)
+                         
+        except:        
+            newfile = supermarket2
+            print "L less than 1"
+            
+        # Write out new data set
+       
+        cols = ['date', 'store', 'ons_item_no', 'ons_item_name',  'product_name', 'new_match', 
+                'item_price_num', 'unit_price', 'units', 'std_price', 'std_unit',  'volume_total',    
+                'num_units', 'sub_type', 'brand', 'range', 'alcohol_volume', 'timestamp', 'product_type', 'promo', 'offer']
+       
+            
+        newfile2 = newfile.ix[:, cols]
+           
+        newfile2.to_csv(fil+ supermarket +'/_longitudinal.csv', index = False)
+        
+        print 'newfile=', newfile2.shape
+    '''
